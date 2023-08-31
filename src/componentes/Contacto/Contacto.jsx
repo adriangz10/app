551d2bd86a46692d1acf4d1d9b6683f3db9276fc
adriangz10 <adrian-gomez@live.com>
2023-08-31 00:46:39 -0300

    Seccion de contactos

--- src/componentes/Contacto/Contacto.jsx
+++ src/componentes/Contacto/Contacto.jsx
@@ -1,28 +1,109 @@
+import { useState } from "react";
+
+import { Form, Button, Card, Table } from "react-bootstrap";
+
 export function Contacto() {
+  const [formulario, setFormulario] = useState({
+    nombre: "",
+    correo: "",
+    mensaje: "",
+  });
+
+  function enviarInformacion() {
+    alert(JSON.stringify(formulario));
+  }
   return (
-    <div className="container mt-5">
-      <div className="textBody">
-        <h1 className="text-center">Informacion de Contacto</h1>
-        <div style={{ marginLeft: "1%" }}>
-          <div style={{ marginRight: "1%" }}>
-            <p align="center">
-              Monseñor Tavella 1424. Concordia. CP(3200).
-              <br />
-              Provincia de Entre Ríos
-              <br />
-              Teléfono: (+54) (345) 4231400
-              <br />
-              Fax: (+54) (345) 4231410
-              <br />
-              E-mail:{" "}
-              <a className="linkText" href="mailto:informes.fcad@uner.edu.ar">
-                informes.fcad@uner.edu.ar
-              </a>
-              <br />
-            </p>
+    <>
+      <div className="container mt-6">
+        <div className="row">
+          <div className="col-md-2"></div>
+          <div className="col-md-4">
+            <Card style={{ width: "26rem" }}>
+              <Card.Body>
+                <Card.Title>Envía tus consultas</Card.Title>
+
+                <Form onSubmit={enviarInformacion}>
+                  <Form.Group className="mb-3" controlId="formBasicNombre">
+                    <Form.Label>Nombre y Apellido</Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={(e) =>
+                        setFormulario({ ...formulario, nombre: e.target.value })
+                      }
+                    />
+                    <Form.Text className="text-muted">
+                      Tu información no va a ser compartida.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicCorreo">
+                    <Form.Label>Correo Electrónico</Form.Label>
+                    <Form.Control
+                      type="email"
+                      onChange={(e) =>
+                        setFormulario({ ...formulario, correo: e.target.value })
+                      }
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicMensaje">
+                    <Form.Label>Dejá tu consulta</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={5}
+                      onChange={(e) =>
+                        setFormulario({
+                          ...formulario,
+                          mensaje: e.target.value,
+                        })
+                      }
+                    />
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Enviar
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="col-md-4">
+            <Card style={{ width: "26rem" }}>
+              <Card.Body>
+                <Card.Title>Información Útil</Card.Title>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Departamento</th>
+                      <th>Teléfono:</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Operadora</td>
+                      <td>421-0001</td>
+                    </tr>
+                    <tr>
+                      <td>Decáno</td>
+                      <td>421-0002</td>
+                    </tr>
+                    <tr>
+                      <td>Pasantías</td>
+                      <td>421-0003</td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <p>
+                  Monseñor Tavella 1424. Concordia. CP(3200). | Provincia de
+                  Entre Ríos Teléfono: (+54) (345) 4231400 – Fax: (+54) (345)
+                  4231410 | E-mail.: informes.fcad@uner.edu.ar
+                </p>
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
