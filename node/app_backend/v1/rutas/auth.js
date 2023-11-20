510365b927d8557984b8337679ce4b90cffc3d3a
adriangz10 <adrian-gomez@live.com>
2023-11-19 22:43:32 -0300

    Ultimos cambios

--- node/app_backend/v1/rutas/auth.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const { login } = require('../../controladores/auth');
-
-const router = Router();
-
-router.post('/login', login);
-
-module.exports = router;
